e2d0043f4c34afa06569b01bfceee56605fc6375
aibrohim <ibrohim7840@gmail.com>
2022-03-08 07:38:36 +0500

    showed count of words on each theme card

--- src/components/theme-card/theme-card.tsx
+++ src/components/theme-card/theme-card.tsx
@@ -8,12 +8,12 @@ interface Props extends Theme {
 
 }
 
-const ThemeCard:FC<Props> = function({id, title}) {
+const ThemeCard:FC<Props> = function({id, title, words_count}) {
   return (
     <article className="theme-card">
       <Link className="theme-card__link" to={"/theme/" + id}>
         <h3 className="theme-card__title">{title}</h3>
-        <p className="theme-card__words"><strong style={{fontWeight: "500"}}>Words: </strong>12</p>
+        <p className="theme-card__words"><strong style={{fontWeight: "500"}}>Words: </strong>{words_count}</p>
       </Link>
     </article>
   );
